4ae16d212686c6baeb4ac6f23ac4eaf3ab7fcbb9
NGUYET PHAN <nguyetpt237@gmail.com>
2022-10-01 01:20:45 +0700

    state

--- src/views/Example/MyComponents.js
+++ src/views/Example/MyComponents.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class MyComponent extends React.Component {
+    //state la 1 object {key:value,...}
+    state = {
+        name: 'Nguyet',
+        age: 22
+    }
+    /*
+    JSX: 
+    - viet js trong html - trong dau {}
+    - funtion javascript tra ve html: return => block (1 phan tu html)
+        su dung fragment neu muon tra ve > 1 block (cu phap: <> .... </>)
+    */
     render() {
+
         return (
-            <div> This is a component.</div>
+            <>
+                <div className="first">
+                    This is a component.
+                    <br></br>
+                    My name is {this.state.name}.
+                </div>
+                <div className="second">
+                    I am {this.state.age}.
+                </div>
+            </>
+
         )
     }
 }
